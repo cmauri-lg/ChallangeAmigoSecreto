01e5b67ab47b4492528dd17cd2215c4253a43911
cmauri-lg <cmauri.lg@gmail.com>
2025-03-16 23:38:45 -0600

    Agregando Array

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+//Variable de tipo array, que almacenará los nombres de los amigos ingresados
+let amigos = [];
+
+
 
 
