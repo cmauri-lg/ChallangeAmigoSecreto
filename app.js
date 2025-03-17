ae1991329021fc2b0d057408570b92313fefbc3e
cmauri-lg <cmauri.lg@gmail.com>
2025-03-16 23:39:50 -0600

    Agregando funcion agregar amigo

--- app.js
+++ app.js
@@ -3,6 +3,27 @@
 //Variable de tipo array, que almacenará los nombres de los amigos ingresados
 let amigos = [];
 
+// Función para agregar amigos
+ function agregarAmigo(){
+    // Capturar el valor del campo de entrada
+    const nombreInput = document.getElementById("amigo");
+    const nombre = nombreInput.value.trim();
+
+        // Validar la entrada
+        if (nombre === "") {
+            alert("Por favor, inserte un nombre.");
+            return;
+        }
+    // Actualizar el array de amigos
+    amigos.push(nombre);
+
+    // Limpiar el campo de entrada
+    nombreInput.value = "";
+
+    // Llamar a la función para actualizar la lista visible en la página (opcional)
+    actualizarLista();
+}
+
 
 
 
